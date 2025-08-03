dc4f4f7fb9d28130368602b1c49c2340b961bec0
laninrariane <75452170+laninrariane@users.noreply.github.com>
2025-08-03 10:24:18 -0500

    Header

---
+++ microservice.py
@@ -0,0 +1,12 @@
+###################################################
+#
+# Stephen Reid
+# 08/03/2025
+# CS361
+# Microservice A
+#
+# Accepts JSON containing email, subject, message, and auth key and sends email 
+# to address based on data received 
+#
+###################################################
+
